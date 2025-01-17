a08bb48af137df54c3f046e610845bd97157809c
Anastasia <155824290+kryzanivska-nastya@users.noreply.github.com>
2025-01-17 20:44:53 +0200

    change model

--- src/app/main/component/comments/models/comments-model.ts
+++ src/app/main/component/comments/models/comments-model.ts
@@ -7,7 +7,7 @@ export interface CommentsModel {
 export interface AuthorDTO {
   id: number;
   name: string;
-  profilePicturePath: null;
+  profilePicturePath: string | null;
 }
 
 export interface CommentsDTO {
